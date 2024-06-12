6a5d071e059bfb2f831e3381a713e3769d9a9daa
Daniel Seiner <51313652+dannyseiner@users.noreply.github.com>
2024-06-12 09:43:10 +0200

    Update DeviceCard.stories.ts

--- src/modules/common/components/DeviceCard/DeviceCard.stories.ts
+++ src/modules/common/components/DeviceCard/DeviceCard.stories.ts
@@ -17,6 +17,8 @@ export const Default: Story = {
       id: "sl213-1flfn-3121j-sfnk23",
       image: "",
       name: "Lilie 1",
+      type: "type",
+      mesurements: [],
     },
     className: "max-w-[66px]",
   },
